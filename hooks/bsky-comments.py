3bc3083717625a28e9f92b786a3cc8be0ec25d24
sergio <sergio@cimarron.io>
2024-12-04 14:37:02 -0800

    Refactor: Move Bluesky comments to separate hook

--- hooks/bsky-comments.py
+++ hooks/bsky-comments.py
@@ -1,7 +1,16 @@
-def on_page_content(html, page, config, files):
-    bsky_post_uri = page.meta.get('bsky_post_uri')
+from textwrap import dedent
+
+
+def on_page_markdown(markdown, **kwargs):
+    page = kwargs["page"]
+    bsky_post_uri = page.meta.get("bsky_post_uri")
+    placeholder_div = "<div />"
     if bsky_post_uri:
-        # Inject a placeholder div with a data attribute
-        placeholder_div = f'<div id="bluesky-comments" data-bsky-post-uri="{bsky_post_uri}" style="margin-top: 20px;">Loading comments...</div>'
-        html = html.replace("</main>", f"{placeholder_div}</main>")
-    return html
+        placeholder_div = f'<div id="bluesky-comments" class="admonition info" data-bsky-post-uri="{bsky_post_uri}" style="margin-top: 20px;">Loading comments...</div>'
+
+    markdown = markdown + dedent(f"""
+
+    {placeholder_div}
+    """)
+
+    return markdown
--- hooks/socialmedia.py
+++ hooks/socialmedia.py
@@ -16,15 +16,10 @@ def on_page_markdown(markdown, **kwargs):
 
     page_url = config.site_url + page.url
     page_title = urllib.parse.quote(page.title + "\n")
-    bsky_post_uri = page.meta.get("bsky_post_uri")
-    placeholder_div = "<div />"
-    if bsky_post_uri: 
-        placeholder_div = f'<div id="bluesky-comments" data-bsky-post-uri="{bsky_post_uri}" style="margin-top: 20px;">Loading comments...</div>'
+
     return markdown + dedent(f"""
     
     [Share on :simple-bluesky:]({bsky_intent}?text={page_title}{page_url}){{ .md-button }}
     [Share on :simple-x:]({x_intent}?text={page_title}&url={page_url}){{ .md-button }}
     [Share on :simple-facebook:]({fb_sharer}?u={page_url}){{ .md-button }}
-
-    {placeholder_div}
     """)
